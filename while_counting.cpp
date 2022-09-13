dc92ab718e9694af88c7cf95cc30d5af5f874f88
Abhijeetx02 <113451409+Abhijeetx02@users.noreply.github.com>
2022-09-13 22:19:05 +0530

    Add files via upload

---
+++ while_counting.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int i=6;
+    string alpahbets[7]= {"a","b","c","d","e","f","g"};
+    while (i>=0)
+    {
+        cout<<alpahbets[i] << "\n";
+        i--;
+    }
+    return 0;
+}
